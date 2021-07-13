5a1b033b542894e6296891406af6cfc190ed4e5f
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-07-13 18:15:41 +0300

    13/07/21

--- +BASІС PYTHON+/+OOP/OOP/16-17. __slots__ for classes.py
+++ +BASІС PYTHON+/+OOP/OOP/16-17. __slots__ for classes.py
@@ -1,6 +1,7 @@
 # "__slots__" задає обмеження на створення нових атрибутів класу,
 # оптимізує пам'\ять, яку займає екземляр,
 # прискорє роботу із локальними методами
+import math
 import timeit
 
 
@@ -53,4 +54,51 @@ print(f"{pt2.__sizeof__() = }bit -- очевидна оптимізація по
 print(f'\n{timeit.timeit(pt1.calc) = }')
 print(
     f'{timeit.timeit(pt2.calc) = } -- оптимізація оптимізація по часу '
-    f'(виграш у {round(timeit.timeit(pt1.calc) - timeit.timeit(pt2.calc), 4)}s)')
+    f'(виграш у {round(timeit.timeit(pt1.calc) - timeit.timeit(pt2.calc), 4)}s)\n')
+
+
+# "__slots__" із property
+# length стає локальною властивістю
+class Point2D:
+    __slots__ = ('x', 'y', '__length')  # через кортеж
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.__length = math.sqrt(x * x + y * y)
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, value):
+        self.__length = value
+
+
+pt = Point2D(1, 2)
+print(f'{pt.length = }')  # тепер можливим є виклик методу length ("__slots__" блокує тільки атрибути, а не методи)
+
+
+class Point3D(Point2D):
+    """Дочірні класи не наслідують "__slots__" """
+    __slots__ = 'z',  # решта у Point2D.__slots__
+
+    def __init__(self, x, y, z):
+        """Конструктор для трьох локальних елементів (два із Point2D)"""
+        super().__init__(x, y)
+        self.z = z
+
+
+p3 = Point3D(1, 1, 1)
+p3.z = 30  # можливість вносити нові атрибути у дочірний клас
+# print(f'{p3.__dict__ = } -- словник дочірного класу, який містить атрибути, які не знаходяться у "__slots__"')
+print(f'{p3.__slots__ = } -- самий "__slots__"\n')
+
+# Із екземпляру дочірного класу можна видалити атрибути із "__slots__", а також їх добавляти (якщо є у "__slots__") :
+del p3.x
+p3.x = 100
+
+print(f"{p3.x = }")
+print(f'{p3.x, p3.y, p3.z = }')
+# p3.d = 1 # введення нового атрибуту видасть AttributeError
