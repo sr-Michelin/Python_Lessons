84f82b54b7af8af22107475bcc159f31a263efad
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-08-03 16:40:44 +0300

    03/08/21

--- +++FOR WORK/__TEST/test_0.py
+++ +++FOR WORK/__TEST/test_0.py
@@ -4,28 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Створення директорії виводу
-if not os.path.isdir(f'OUT'):
-    os.mkdir(f'OUT')
-if not os.path.isdir(f'OUT/plots'):
-    os.mkdir(f'OUT/plots')
+if not os.path.isdir(f'plots'):
+    os.mkdir(f'plots')
+if not os.path.isdir(f'plots/low_yield'):
+    os.mkdir(f'plots/low_yield')
+if not os.path.isdir(f'plots/full_yield'):
+    os.mkdir(f'plots/full_yield')
 
 P = pd.read_excel('Task#2 statistical task.xlsx', sheet_name=1)
 print(P.columns)
 
 # Верхня межа для вибірки "yield"
-N = 1.0
+N = [.9, 1.0]
 
-for y in ['f_r', 'yield', 'lost', 'reject']:
-    # Побудова boxplot для знаходження колреляції між 'testSystem' та чисельними значеннями колонок
-    P[P['yield'] <= N].boxplot(by='testSystem', column=y, grid=True, fontsize=10, figsize=(15, 7))
-    plt.title(f'Task 2\nyield<={N}', fontsize=10)
-    plt.suptitle('')
+for n in N:
+    for y in ['f_r', 'yield', 'lost', 'reject']:
+        # Побудова boxplot для знаходження колреляції між 'testSystem' та чисельними значеннями колонок
+        P[P['yield'] <= n].boxplot(by='testSystem', column=y, grid=True, fontsize=10, figsize=(15, 7))
+        plt.suptitle('')
 
-    plt.xlabel('testSystem')
-    if y == 'f_r':
-        plt.ylabel('finishedDate-releaseDate')
-    else:
-        plt.ylabel(y)
+        plt.xlabel('testSystem')
+        if y == 'f_r':
+            plt.title(f'Task 2: finishedDate-releaseDate\nyield<={n}', fontsize=10)
+            plt.ylabel('finishedDate-releaseDate')
+        else:
+            plt.ylabel(y)
+            plt.title(f'Task 2: {y}\nyield<={n}', fontsize=10)
 
-    plt.savefig(f'OUT/plots/testSystem_{y}_yield_l{N}.png')
-    plt.show()
+        if n == N[0]:
+            plt.savefig(f'plots/low_yield/testSystem_{y}_yield_l{n}.png')
+        else:
+            plt.savefig(f'plots/full_yield/testSystem_{y}_yield_l{n}.png')
+        # plt.show()
+
+
+
+
