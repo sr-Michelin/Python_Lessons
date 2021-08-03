068b30b91e1377db4283406ed7a8e45df46e0d94
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-08-03 18:42:34 +0300

    03/08/21

--- +++FOR WORK/__TEST/test_0.py
+++ +++FOR WORK/__TEST/test_0.py
@@ -12,10 +12,11 @@ if not os.path.isdir(f'plots/full_yield'):
     os.mkdir(f'plots/full_yield')
 
 P = pd.read_excel('Task#2 statistical task.xlsx', sheet_name=1)
-print(P.columns)
+testSystem_ = sorted(list(set(P['testSystem'])))
+# print(P.columns)
 
 # Верхня межа для вибірки "yield"
-N = [.9, 1.0]
+N = [0.9, 1.0]
 
 for n in N:
     for y in ['f_r', 'yield', 'lost', 'reject']:
@@ -35,8 +36,26 @@ for n in N:
             plt.savefig(f'plots/low_yield/testSystem_{y}_yield_l{n}.png')
         else:
             plt.savefig(f'plots/full_yield/testSystem_{y}_yield_l{n}.png')
-        # plt.show()
+        plt.show()
 
+    print(f'\nКореляції Пірсона для межі "yield" <= {n}:')
+    print(f'P_corr(yield, f_r): {np.corrcoef(P[P["yield"] <= n]["yield"], P[P["yield"] <= n]["f_r"])[0][1]} '
+          f'- ймовірна')
+    print(f'P_corr(yield, lost): {np.corrcoef(P[P["yield"] <= n]["yield"], P[P["yield"] <= n]["lost"])[0][1]}')
+    print(f'P_corr(yield, reject): {np.corrcoef(P[P["yield"] <= n]["yield"], P[P["yield"] <= n]["reject"])[0][1]} '
+          f'- надто очевидна')
+    print(f'P_corr(reject, lost): {np.corrcoef(P[P["yield"] <= n]["reject"], P[P["yield"] <= n]["lost"])[0][1]}')
 
-
-
+print(f'\nСтатистики тестових с-м для значення "yield" <= {N[1]}')
+pp_yield = [i for i, j in P[P['yield'] <= N[1]].groupby(['testSystem', 'yield'])]
+yield_q_75 = []
+for t in range(len(testSystem_)):
+    d_pp = {testSystem_[t]: [j for i, j in pp_yield if i == testSystem_[t]]}
+    yield_q_75.append(np.quantile(list(d_pp.values())[0], q=.75))
+    print(
+        f'{testSystem_[t]}: min = {round(min(list(d_pp.values())[0]), 3)}, '
+        f'max = {round(max(list(d_pp.values())[0]), 3)}, '
+        f'mean = {round(float(np.mean(list(d_pp.values())[0])), 3)}, '
+        f'std = {round(float(np.std(list(d_pp.values())[0])), 3)}, '
+        f'q_25 = {round(np.quantile(list(d_pp.values())[0], .25), 3)}, '
+        f'q_75 = {round(np.quantile(list(d_pp.values())[0], .75), 3)}')
