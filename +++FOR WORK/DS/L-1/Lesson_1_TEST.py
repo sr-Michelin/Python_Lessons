e07ab1d588cf67ea9a1d81a344cbcc03e1577bd0
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-08-07 14:08:35 +0300

    07/08/21

--- +++FOR WORK/DS/L-1/Lesson_1_TEST.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race',
-# 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary']
-P = pd.read_csv('adult.data', sep=',')
-
-print(P.pivot_table(['age', 'hours-per-week', 'capital-loss'], ['marital-status'], aggfunc='mean'))
