2e4e1b860dc70387f01f3091a0b8fa17a6822d2f
Mike Sh <71037546+sr-Michelin@users.noreply.github.com>
2021-08-13 19:51:37 +0300

    13/08/21

--- +MODULES (separate)+/+Ppyttsx3/Helsreach.py
+++ +MODULES (separate)+/+Ppyttsx3/Helsreach.py
@@ -2,18 +2,20 @@ import pyttsx3
 
 
 def read_text(name):
-    with open(name, 'r', encoding='utf-8') as file:
+    with open(name, 'r', encoding='windows-1251') as file:
         text = file.read()
         return text
 
 
 engine = pyttsx3.init()
-voice = engine.getProperty('voices')[1]
-engine.setProperty('rate', 140)
+voice = engine.getProperty('voices')[0]
+engine.setProperty('rate', 160)
 print(voice.name)
 
 engine.setProperty('voice', voice.id)
 
-engine.say(read_text(name='Dembski-Bowden Aaron. Helsreach - royallib.ru.txt'))
+# engine.say(read_text(name='data/Dembski-Bowden Aaron. Helsreach - royallib.ru.txt'))
+engine.say(read_text(name='data/Dembski-Bouden_Helsrich_RuLit_Net.txt'))
+
 engine.runAndWait()
 engine.stop()
