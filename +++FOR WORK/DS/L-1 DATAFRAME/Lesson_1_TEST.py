1cd7cd3fd87ff2a9bedeef21556ab3710c939ff9
miker <mikeraptore@gmail.com>
2021-09-19 19:40:39 +0300

    19/09/21

--- +++FOR WORK/DS/L-1 DATAFRAME/Lesson_1_TEST.py
+++ +++FOR WORK/DS/L-1 DATAFRAME/Lesson_1_TEST.py
@@ -4,4 +4,4 @@ import pandas as pd
 # 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary']
 P = pd.read_csv('adult.data', sep=',')
 
-print(P.pivot_table(['age', 'hours-per-week', 'capital-loss'], ['marital-status'], aggfunc='mean'))
+print(P.pivot_table(['age', 'hours-per-week', 'capital-loss', 'salary'], ['education'], aggfunc='max'))
