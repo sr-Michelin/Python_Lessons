22fa5a73a88a092d1b3390e90af9d7846a605a13
miker <mikeraptore@gmail.com>
2021-09-25 18:12:54 +0300

    24/09/21

---
+++ +++FOR WORK/__TEST/test_0.py
@@ -0,0 +1,24 @@
+import sqlite3 as sq
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+with sq.connect('data.db') as con:
+    cur = con.cursor()
+
+    # cur.execute("""CREATE TABLE if not exists users(
+    # name TEXT,
+    # sex INT,
+    # age INT,
+    # score INT
+    # )""")
+
+    cur.execute("""SELECT * FROM users""")
+    # result = cur.fetchall()
+    # result = cur.fetchmany(2)
+
+    # result = cur.fetchone()
+    print(result)
+
+
+df = pd.read_sql(sql='SELECT * FROM users WHERE score >= 0', con=sq.connect('data.db'))
