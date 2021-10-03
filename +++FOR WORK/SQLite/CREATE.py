970da28a16e230ce222304802b98a9d5bd35565c
miker <mikeraptore@gmail.com>
2021-10-03 20:19:31 +0300

    03/10/21

--- +++FOR WORK/SQLite/CREATE.py
+++ +++FOR WORK/SQLite/CREATE.py
@@ -18,16 +18,15 @@ with sqlite3.connect('data.db') as con:
 
     # Вивід запиту у консоль
     curs.execute('SELECT DISTINCT * FROM train;')
-    print(curs.fetchall())
+    print("DISTINCT: ",curs.fetchall())
 
     # Агрегування ф-ціями для чиселних колонок
     curs.execute('SELECT avg(id) AS avg_id, avg(course) AS avg_c FROM train')
-    print(curs.fetchall())
+    print("Aggregate: ",curs.fetchall())
 
     # Виділення певних строк
     curs.execute('SELECT * FROM train LIMIT 3')
-
-    print(curs.fetchall())
+    print("LIMIT: ", curs.fetchall())
 
     # SELECT DISTINCT [columns] - вивід унікальних значень таблиці
 
@@ -38,10 +37,21 @@ with sqlite3.connect('data.db') as con:
     # Оновлення існуючих записів у таблиці з первною умовою (без неї усі записи стануть одинаковими)
     curs.execute('UPDATE train SET name = "Mike Sh" WHERE name = "Mike"')
 
+    # LIKE Operator - пошук спеціальних символів та їх косбінацій у текстах колонок
+    # 'a%' - start with "a"
+    # '%a' - end with "a"
+    # '%or%' - have "or" in any position
+    # '_r%' - have "r" in the second position
+    # 'a_%' - start with "a" and are at least 2 characters in length
+    # 'a%o' - start with "a" and ends with "o"
+    curs.execute('SELECT name FROM train WHERE name LIKE "M%h";')
+    print("LIKE: ",curs.fetchall())
+
     # -----------------------------------------------------------------------------------------------------------------
     # Вивід через бібліотеку Pandas
     p = pd.read_sql('SELECT * FROM train ORDER BY course DESC', con=sqlite3.connect('data.db'))
     print('\n', p)
 
-    p = pd.read_sql('SELECT avg(id) AS avg_id, avg(course) AS avg_c FROM train', con=sqlite3.connect('data.db'))
+    p = pd.read_sql('SELECT COUNT(id) AS count, avg(id) AS avg_id, avg(course) AS avg_c FROM train',
+                    con=sqlite3.connect('data.db'))
     print('\n', p)
