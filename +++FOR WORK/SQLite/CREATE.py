a6ca4142e809331091adff9ff3b901baff73d66c
miker <mikeraptore@gmail.com>
2021-10-03 19:08:16 +0300

    03/10/21

---
+++ +++FOR WORK/SQLite/CREATE.py
@@ -0,0 +1,30 @@
+import sqlite3
+import pandas as pd
+
+with sqlite3.connect('data.db') as con:
+    curs = con.cursor()
+
+    # Створення таблиці
+    curs.execute('CREATE TABLE IF NOT EXISTS train('
+                 'id INT PRIMARY KEY AUTOINCREMENT,'
+                 'name TEXT NOT NULL,'
+                 'course INT NOT NULL CHECK (course BETWEEN 0 AND 6));')
+
+    # Введення даних
+    # curs.execute('INSERT INTO train(id, name, course) VALUES(9, "Kata", 5)')
+
+    # Видалення даних
+    curs.execute('DELETE FROM train WHERE id IS Null;')
+
+    # Вивід запиту у консоль
+    curs.execute('SELECT * FROM train;')
+    print(curs.fetchall())
+
+    # WHERE [condition] - умова виводу
+    # GROUP BY [column] - групування записів у таблиці (під час виводу)
+    # ORDER BY [column] ASC|DESC - сортування за колонкою(-ами) у напрямку зростання чи спадання
+
+    # -----------------------------------------------------------------------------------------------------------------
+    # Вивід через бібліотеку Pandas
+    p = pd.read_sql('SELECT * FROM train ORDER BY course DESC', con=sqlite3.connect('data.db'))
+    print('\n', p.describe().T)
