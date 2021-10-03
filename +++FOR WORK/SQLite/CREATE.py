de9bb0556884419cc7ed88b65345f6949af5218b
miker <mikeraptore@gmail.com>
2021-10-03 19:49:12 +0300

    03/10/21

--- +++FOR WORK/SQLite/CREATE.py
+++ +++FOR WORK/SQLite/CREATE.py
@@ -17,7 +17,11 @@ with sqlite3.connect('data.db') as con:
     curs.execute('DELETE FROM train WHERE id IS Null;')
 
     # Вивід запиту у консоль
-    curs.execute('SELECT * FROM train;')
+    curs.execute('SELECT DISTINCT * FROM train;')
+    print(curs.fetchall())
+
+    # Агрегування ф-ціями для чиселних колонок
+    curs.execute('SELECT avg(id), avg(course) FROM train')
     print(curs.fetchall())
 
     # SELECT DISTINCT [columns] - вивід унікальних значень таблиці
@@ -25,7 +29,13 @@ with sqlite3.connect('data.db') as con:
     # GROUP BY [column] - групування записів у таблиці (під час виводу)
     # ORDER BY [column] ASC|DESC - сортування за колонкою(-ами) у напрямку зростання чи спадання
 
+    # Оновлення існуючих записів у таблиці з первною умовою (без неї усі записи стануть одинаковими)
+    curs.execute('UPDATE train SET name = "Mike Sh" WHERE name = "Mike"')
+
     # -----------------------------------------------------------------------------------------------------------------
     # Вивід через бібліотеку Pandas
     p = pd.read_sql('SELECT * FROM train ORDER BY course DESC', con=sqlite3.connect('data.db'))
-    print('\n', p.describe().T)
+    print('\n', p)
+
+    p = pd.read_sql('SELECT avg(id), avg(course) FROM train', con=sqlite3.connect('data.db'))
+    print('\n', p)
