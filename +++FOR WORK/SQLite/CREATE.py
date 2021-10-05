7867679ceb760c1a34f782377befa15cae424528
miker <mikeraptore@gmail.com>
2021-10-05 23:00:41 +0300

    05/10/21

--- +++FOR WORK/SQLite/CREATE.py
+++ +++FOR WORK/SQLite/CREATE.py
@@ -10,8 +10,14 @@ with sqlite3.connect('data.db') as con:
                  'name TEXT NOT NULL,'
                  'course INT NOT NULL CHECK (course BETWEEN 0 AND 6));')
 
+    # Створення таблиці ssh(стипендій)
+    curs.execute('CREATE TABLE IF NOT EXISTS ssh('
+                 'id INT AUTO INCREMENT PRIMARY KEY,'
+                 'sh INT NOT NULL);')
+
     # Введення даних
     # curs.execute('INSERT INTO train(id, name, course) VALUES(9, "Kata", 5)')
+    # curs.execute('INSERT INTO ssh(id, sh) VALUES(1, 1770), (2, 1770), (3, 0), (4, 0), (5,0), (6, 0), (7, 0)')
 
     # Видалення даних
     curs.execute('DELETE FROM train WHERE id IS Null;')
@@ -51,12 +57,32 @@ with sqlite3.connect('data.db') as con:
     curs.execute('SELECT * FROM train WHERE name IN ("Mike Sh", "Taras", "Vanya")')
     print("IN: ", curs.fetchall())
 
+    # JOINs - входження таблиць. Для троьох та більше працює вкладення.
+    # # INNER JOIN - спільна область для двох таблиць
+    curs.execute('SELECT train.name, train.course, ssh.sh FROM train INNER JOIN ssh ON train.id=ssh.id')
+    print('INNER JOIN:', curs.fetchall())
+    # # LEFT JOIN - повна ліва (зазначена першою) таблиця і співпадаючі дані із правої
+    curs.execute('SELECT train.name, train.course, ssh.sh FROM train LEFT JOIN ssh ON train.id=ssh.id')
+    print('LEFT JOIN:', curs.fetchall())
+    # # RIGHT JOIN - повна права таблиця і співпадаючі дані із лівої
+    # curs.execute('SELECT train.name, train.course, ssh.sh FROM train RIGHT JOIN ssh ON train.id=ssh.id')
+    # print('RIGHT JOIN:', curs.fetchall())
+    # # FULL JOIN - усі дані
+    # curs.execute('SELECT train.name, train.course, ssh.sh FROM train FULL JOIN ssh ON train.id=ssh.id')
+    # print('FULL JOIN:', curs.fetchall())
 
-    # -----------------------------------------------------------------------------------------------------------------
+    # UNION - оператор поєднання вибірок
+    curs.execute('SELECT COUNT(*) FROM train as CNT_tr UNION SELECT COUNT(*) FROM ssh as CNT_ssh')
+    print('UNION:', curs.fetchall())
+
+    #   -----------------------------------------------------------------------------------------------------------------
     # Вивід через бібліотеку Pandas
-    p = pd.read_sql('SELECT * FROM train ORDER BY course DESC', con=sqlite3.connect('data.db'))
-    print('\n', p)
+    print('\n', pd.read_sql('SELECT * FROM train ORDER BY course DESC', con=con))
 
-    p = pd.read_sql('SELECT COUNT(id) AS count, avg(id) AS avg_id, avg(course) AS avg_c FROM train',
-                    con=sqlite3.connect('data.db'))
-    print('\n', p)
+    print('\n', pd.read_sql('SELECT COUNT(id) AS count, avg(id) AS avg_id, avg(course) AS avg_c FROM train', con=con))
+
+    print('\n', pd.read_sql(sql='SELECT train.name, train.course, ssh.sh FROM train INNER JOIN ssh ON train.id=ssh.id',
+                            con=con))
+
+    print('\n', pd.read_sql(sql='SELECT COUNT(*) FROM train as CNT_tr UNION SELECT COUNT(*) FROM ssh as CNT_ssh',
+                            con=con))
--- +++FOR WORK/__TEST/test_1.py
+++ +++FOR WORK/__TEST/test_1.py
@@ -4,11 +4,11 @@ import pandas as pd
 with sqlite3.connect('interview.db') as con:
     cursor = con.cursor()
 
-    cursor.execute("""INSERT INTO train(id,name, age, salary) VALUES(1,'Mike', 22, 5000)""")
+    cursor.execute("""INSERT INTO train(id,name, age, salary) VALUES(4,'Mike', 22, 5000)""")
 
     cursor.execute("""DELETE FROM train 
     WHERE id NOT IN (SELECT MIN(ID) FROM train GROUP BY name, age, salary)
     OR id IS NULL;""")
 
-print('\n', pd.read_sql(sql="""SELECT * FROM train""", con=sqlite3.connect('interview.db')))
+    print('\n', pd.read_sql(sql="""SELECT * FROM train""", con=con))
 
